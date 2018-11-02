85af6c1ca3b1b80a8614866e4d24f72b1a2116e8
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-11-02 02:23:31 +0000

    Success

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const mongoose = require('mongoose');
 const express = require('express');
-const app = express.app();
+const app = express();
 
-app.get('/user', (req, res) => {
-    res.send('Hello World')
+app.get('/', (req, res) => {
+    res.send('Hey how are you!');
+});
+
+app.get('/user/:name', (req, res) => {
+    res.send('Hey ' + req.params.name + ' You good brother' + '!')
 })
+
+app.listen(process.env.PORT, process.env.IP, 3000, function(){
+    console.log('Listening to Port 3000')});
